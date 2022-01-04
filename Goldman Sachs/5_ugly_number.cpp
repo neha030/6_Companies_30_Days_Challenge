5c7f783dbbc4cd878c229623df490735dff1fd34
neha030 <nehajha12150@gmail.com>
2022-01-04 19:27:35 +0530

    added

--- Goldman Sachs/5_ugly_number.cpp
+++ Goldman Sachs/5_ugly_number.cpp
@@ -4,3 +4,27 @@ Program to find Nth Ugly Number.
 
 #include <bits/stdc++.h>
 using namespace std;
+
+int ugly (int n)
+{
+   set <int> s;
+	   s.insert(1);
+	   n--;
+	   while(n--){
+	        auto it = s.begin();
+	        long long int x = *it;
+	        s.erase(it);
+	        s.insert(x*2);
+	        s.insert(x*3);
+	        s.insert(x*5);
+	    } 
+        return *s.begin();
+}
+
+
+int main()
+{
+    int n=10;
+    cout<<ugly(n);
+    return 0;
+}
