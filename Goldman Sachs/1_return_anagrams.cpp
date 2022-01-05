7d2997ddb9a78b61e4ee8353000ccff51a30a8ca
neha030 <nehajha12150@gmail.com>
2022-01-05 12:34:51 +0530

    added

--- Goldman Sachs/1_return_anagrams.cpp
+++ Goldman Sachs/1_return_anagrams.cpp
@@ -5,15 +5,36 @@ Given an array of strings, return all groups of strings that are anagrams.
 #include <bits/stdc++.h>
 using namespace std;
 
-void printAnagram(char* arr[], int n)
-{
+#include <bits/stdc++.h>
+using namespace std;
 
+
+vector<vector<string>> Anagrams(vector<string>& string_list) {
+    unordered_map<string, vector<string>> m;
+    for(auto & x : string_list) {
+        string a = x;
+        std::sort(x.begin(), x.end());
+        m[x].push_back(a);
+    }
+    vector<vector<string>> ans(m.size());
+    for (const auto& mm:m) {
+        auto v = mm.second;
+        ans.push_back(v);
+    }
+    return ans;
 }
 
-int main ()
-{
-    char* arr={"cat", "dog", "tac", "god", "act", "gdo"};
-    int n=sizeof(arr)/sizeof(arr[0]);
-    printAnagram(arr,n);
+int main() {
+    vector<vector<string>> intervals;
+    vector <string>words = {"act","god","cat","dog","tac"};
+
+    intervals = Anagrams(words);
+
+    for(const auto& interval:intervals){
+    for(const auto& num:interval)
+    {
+        cout<<num<<" ";
+    }
+}
     return 0;
 }
--- Goldman Sachs/8_decoding_message.cpp
+++ Goldman Sachs/8_decoding_message.cpp
@@ -5,34 +5,52 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int CountWays(string s)
-{
-    if (s.size() < 1)
-        return 0;
-    if (s[0] == '0')
-        return 0;
-    if (s.size() == 1)
-        return 1;
-    int l1 = 1;
-    int l2 = 1;
-    for (int i = 1; i < s.size(); i++)
-    {
-        int d1 = s[i] - '0';
-        int d2 = (s[i - 1] - '0') * 10 + d1;
-        int val = 0;
-        if (d1 >= 1)
-            val = (val + l2) % mod;
-        if (d2 > 9 && d2 <= 26)
-            val = (val + l1) % mod;
-        l1 = l2;
-        l2 = val;
-    }
-    return l2 % mod;
-}
+class Solution {
+	public:
+		int CountWays(string str){
+	      int n=str.size();
+		    long long int mod=1000000007;
+		    int dp[n+1];
+		    dp[0]=1;
+		    dp[1]=1;
+		    if(str[0]=='0')
+		    return 0;
+		    for(int i=2;i<=n;i++){
+		        if(str[i-1]=='0' and str[i-2]!='0')
+		        {
+		            int num=stoi(str.substr(i-2,2));
+		            if(num>26)
+		            dp[i]=0;
+		            else
+		            dp[i]=dp[i-2]%mod;
+		        }
+		        else if(str[i-1]!='0' and str[i-2]=='0')
+		        dp[i]=dp[i-1]%mod;
+		        else if(str[i-1]=='0' and str[i-2]=='0')
+		        dp[i]=0;
+		        else{
+		            int num=stoi(str.substr(i-2,2));
+		            if(num>26)
+		            dp[i]=dp[i-1]%mod;
+		            else
+		            dp[i]=(dp[i-1]+dp[i-2])%mod;
+		        }
+		        
+		    }
+		    return dp[n];
+		}
 
-int main()
-{
-    string s = "123";
-    cout << CountWays(s);
-    return 0;
-}
+};
+
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		string str;
+		cin >> str;
+		Solution obj;
+		int ans = obj.CountWays(str);
+		cout << ans << "\n";
+	}
+	return 0;
+} 
