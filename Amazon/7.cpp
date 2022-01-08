13c011f8fb2837ef42854fa0dc984de0eda1a25d
neha030 <nehajha12150@gmail.com>
2022-01-08 11:53:43 +0530

    assed

---
+++ Amazon/7.cpp
@@ -0,0 +1,34 @@
+class Solution
+{
+public:
+    string FirstNonRepeating(string A)
+    {
+        // Code here
+        vector<int> vis(26, 0);
+        string ans = "";
+        vector<char> v;
+        int n = A.length();
+        for (int i = 0; i < n; i++)
+        {
+            if (!vis[A[i] - 'a'])
+            {
+                v.push_back(A[i]);
+            }
+            vis[A[i] - 'a']++;
+            int f = 0;
+            int m = v.size();
+            for (int j = 0; j < m; j++)
+            {
+                if (vis[v[j] - 'a'] == 1)
+                {
+                    ans += v[j];
+                    f = 1;
+                    break;
+                }
+            }
+            if (f == 0)
+                ans += '#';
+        }
+        return ans;
+    }
+};
