1c09fa60a8e52d20d6d4b507d6892e6771be1e72
neha030 <nehajha12150@gmail.com>
2022-01-10 00:09:21 +0530

    added

---
+++ Amazon/8.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+public:
+    // Function to count number of ways to reach the nth stair
+    // when order does not matter.
+    long long countWays(int n)
+    {
+        // your code here
+        vector<long long> dp(n + 1, -1);
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++)
+        {
+            dp[i] = dp[i - 2] + 1;
+        }
+        return dp[n];
+    }
+};
