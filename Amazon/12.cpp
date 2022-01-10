8fce858f0cdeaaf7d9143d7c5d3df08c22a80439
neha030 <nehajha12150@gmail.com>
2022-01-10 13:27:57 +0530

    added

---
+++ Amazon/12.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+public:
+    string colName(long long int n)
+    {
+        // your code here
+        string res;
+        char tmp;
+        while (n)
+        {
+            n -= 1;
+            tmp = 'A' + n % 26;
+            res = tmp + res;
+            n /= 26;
+        }
+        return res;
+    }
+};
