54e1b6312087e4e44eeaf1c1b4d14cc5c7a69744
neha030 <nehajha12150@gmail.com>
2022-01-15 01:51:09 +0530

    added

--- Microsoft/1.cpp
+++ Microsoft/1.cpp
@@ -2,3 +2,46 @@
 Divide an array into two sets S1 and S2 such that the absolute difference 
 between their sums is minimum and find the minimum difference
 */
+
+class Solution
+{
+
+public:
+    int minDifference(int arr[], int n)
+    {
+        // Your code goes here
+        int sum = 0;
+        for (int i = 0; i < n; i++)
+            sum += arr[i];
+
+        bool T[n + 1][sum + 1];
+        for (int i = 0; i <= n; i++)
+        {
+            for (int j = 0; j <= sum; j++)
+            {
+                T[i][j] = false;
+            }
+        }
+
+        for (int i = 0; i <= n; i++)
+        {
+            T[i][0] = true;
+            for (int j = 1; i > 0 && j <= sum; j++)
+            {
+                T[i][j] = T[i - 1][j];
+
+                if (arr[i - 1] <= j)
+                {
+                    T[i][j] |= T[i - 1][j - arr[i - 1]];
+                }
+            }
+        }
+
+        int j = sum / 2;
+        while (j >= 0 && !T[n][j])
+        {
+            j--;
+        }
+        return sum - 2 * j;
+    }
+};
--- Microsoft/6.cpp
+++ Microsoft/6.cpp
@@ -1,3 +1,35 @@
 /* Question
 Possible Words From Phone Digits
 */
+
+class Solution
+{
+public:
+    vector<string> pad = {
+        "", "", "abc", "def", "ghi", "jkl",
+        "mno", "pqrs", "tuv", "wxyz"};
+    // Function to find list of all words possible by pressing given numbers.
+    vector<string> possibleWords(int a[], int N)
+    {
+        // Your code here
+        if (N == 0)
+            return {};
+        vector<string> result;
+        result.push_back("");
+
+        for (int i = 0; i < N; i++)
+        {
+            vector<string> tmp;
+            for (auto candidate : pad[a[i]])
+            {
+                for (auto s : result)
+                {
+                    tmp.push_back(s + candidate);
+                }
+            }
+            result.swap(tmp);
+        }
+        sort(result.begin(), result.end());
+        return result;
+    }
+};
