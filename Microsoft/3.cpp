f262cd55fcee7ecd1b86d13776e5f89c085fe1f9
neha030 <nehajha12150@gmail.com>
2022-01-14 13:55:40 +0530

    added

--- Microsoft/3.cpp
+++ Microsoft/3.cpp
@@ -1,3 +1,21 @@
 /* Question
 Rotate by 90 degree
 */
+
+void rotate(vector<vector<int>> &matrix)
+{
+    // Your code goes here
+    int n = matrix.size();
+    int j = 0;
+    for (int i = 0; i < n; i++)
+    {
+        reverse(matrix[i].begin(), matrix[i].end());
+    }
+
+    while (j < n)
+    {
+        for (int i = j; i < n; i++)
+            swap(matrix[j][i], matrix[i][j]);
+        j++;
+    }
+}
