f11e85077eb6e6ce5060c0151f04718e6003fef5
neha030 <nehajha12150@gmail.com>
2022-01-15 01:56:42 +0530

    added

--- Microsoft/10.cpp
+++ Microsoft/10.cpp
@@ -1,3 +1,25 @@
 /* Question
 Stickler Thief (Similar to Alibaba and Thiefes Question)
 */
+
+class Solution
+{
+public:
+    // Function to find the maximum money the thief can get.
+    int FindMaxSum(int arr[], int n)
+    {
+        // Your code here
+        if (n == 0)
+            return 0;
+        int dp[n];
+        if (n >= 1)
+            dp[0] = arr[0];
+        if (n >= 2)
+            dp[1] = max(arr[0], arr[1]);
+        for (int i = 2; i < n; i++)
+        {
+            dp[i] = max(dp[i - 1], dp[i - 2] + arr[i]);
+        }
+        return dp[n - 1];
+    }
+};
--- Microsoft/11.cpp
+++ Microsoft/11.cpp
@@ -1,3 +1,23 @@
 /* Question
 Generate and print all binary numbers with decimal values from 1 to N.
 */
+
+vector<string> generate(int N)
+{
+    // Your code here
+    vector<string> v;
+    queue<string> q;
+    q.push("1");
+
+    while (N--)
+    {
+        string s1 = q.front();
+        q.pop();
+        v.push_back(s1);
+
+        string s2 = s1;
+        q.push(s1.append("0"));
+        q.push(s2.append("1"));
+    }
+    return v;
+}
