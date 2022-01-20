2d69b22f9e66b4c87b3951fc35368bfe44c31df3
neha030 <nehajha12150@gmail.com>
2022-01-20 10:54:37 +0530

    added

--- Adobe/1.cpp
+++ Adobe/1.cpp
@@ -1,3 +1,33 @@
 /* Question
 Find a continuous sub-array which adds to a given number S.
 */
+
+class Solution
+{
+public:
+    // Function to find a continuous sub-array which adds up to a given number.
+    vector<int> subarraySum(int arr[], int n, long long s)
+    {
+        // Your code here
+        int i = 0, j = 0;
+        int cur_sum = arr[0];
+        while (i < n && j < n)
+        {
+            if (cur_sum < s)
+            {
+                j++;
+                cur_sum += arr[j];
+            }
+            else if (cur_sum > s)
+            {
+                cur_sum -= arr[i];
+                i++;
+            }
+            else
+            {
+                return {i + 1, j + 1};
+            }
+        }
+        return {-1};
+    }
+};
--- Adobe/2.cpp
+++ Adobe/2.cpp
@@ -1,3 +1,31 @@
 /* Question
 Find the length of the Longest Arithmetic Progression (LLAP) in it.
 */
+
+class Solution
+{
+public:
+    int lengthOfLongestAP(int A[], int n)
+    {
+        // code here
+        if (n <= 2)
+            return n;
+        vector<unordered_map<int, int>> dp(n);
+        int mx = 2;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                int diff = A[j] - A[i];
+                if (dp[i].find(diff) != dp[i].end())
+                {
+                    dp[j][diff] = dp[i][diff] + 1;
+                }
+                else
+                    dp[j][diff] = 2;
+                mx = max(mx, dp[j][diff]);
+            }
+        }
+        return mx;
+    }
+};
