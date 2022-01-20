1bbe0b847626cd58f1c20664721709548336915a
neha030 <nehajha12150@gmail.com>
2022-01-20 10:57:27 +0530

    added

--- Adobe/11.cpp
+++ Adobe/11.cpp
@@ -1,3 +1,29 @@
 /* Question
 String Amendment
 */
+
+class Solution
+{
+public:
+    string amendSentence(string s)
+    {
+        // your code here
+        if (isupper(s[0]))
+            s[0] = tolower(s[0]);
+        string ans = "";
+        int i = 0, j = 0;
+        while (j < s.length())
+        {
+            if (isupper(s[j]))
+            {
+                s[j] = tolower(s[j]);
+                ans += s.substr(i, j - i);
+                ans += " ";
+                i = j;
+            }
+            j++;
+        }
+        ans += s.substr(i, j - i);
+        return ans;
+    }
+};
--- Adobe/12.cpp
+++ Adobe/12.cpp
@@ -1,3 +1,23 @@
 /* Question
 Leaders in Array
 */
+
+class Solution
+{
+    // Function to find the leaders in the array.
+public:
+    vector<int> leaders(int a[], int n)
+    {
+        // Code here
+        vector<int> ans;
+        int cur_mx = a[n - 1];
+        for (int i = n - 1; i >= 0; i--)
+        {
+            cur_mx = max(cur_mx, a[i]);
+            if (cur_mx == a[i])
+                ans.push_back(a[i]);
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
