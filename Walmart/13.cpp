4f2916604c3594fa90f7f5da77e2dbdb29286e21
neha030 <nehajha12150@gmail.com>
2022-01-31 01:17:29 +0530

    added

--- Walmart/13.cpp
+++ Walmart/13.cpp
@@ -1,3 +1,23 @@
 /* Question
 Find the Kth Largest Integer in the Array (Cows and Farm Question)
 */
+
+class Solution
+{
+public:
+    static bool cmp(string &a, string &b)
+    {
+        if (a.size() == b.size())
+        {
+            return a < b;
+        }
+        return a.size() < b.size();
+    }
+
+    string kthLargestNumber(vector<string> &nums, int k)
+    {
+        sort(nums.begin(), nums.end(), cmp);
+        int n = nums.size();
+        return nums[n - k];
+    }
+};
