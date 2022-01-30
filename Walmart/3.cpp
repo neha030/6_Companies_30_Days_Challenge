d8406086f30b2cc572d4a72909e89419a92ea718
neha030 <nehajha12150@gmail.com>
2022-01-30 22:52:22 +0530

    added

--- Walmart/3.cpp
+++ Walmart/3.cpp
@@ -1,3 +1,21 @@
 /* Question
 https://leetcode.com/problems/remove-colored-pieces-if-both-neighbors-are-the-same-color (Similar to Card Game Question)
 */
+
+class Solution
+{
+public:
+    bool winnerOfGame(string colors)
+    {
+        int alice = 0, bob = 0;
+        int n = colors.length();
+        for (int i = 1; i < n - 1; i++)
+        {
+            if (colors[i - 1] == 'A' && colors[i] == 'A' && colors[i + 1] == 'A')
+                alice++;
+            else if (colors[i - 1] == 'B' && colors[i] == 'B' && colors[i + 1] == 'B')
+                bob++;
+        }
+        return alice > bob;
+    }
+};
--- Walmart/8.cpp
+++ Walmart/8.cpp
@@ -1,3 +1,20 @@
 /* Question
 https://practice.geeksforgeeks.org/problems/maximum-height-tree4803/1/
 */
+
+class Solution
+{
+public:
+    int height(int N)
+    {
+        // code here
+        int i = 1;
+        int res = 1;
+        while (res <= N)
+        {
+            res += i + 1;
+            i += 1;
+        }
+        return i - 1;
+    }
+};
